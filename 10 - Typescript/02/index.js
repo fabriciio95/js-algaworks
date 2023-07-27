32a3c050f28d2f475c0f8af4455ba61ae8f86579
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-07-26 21:03:32 -0300

    Tipando variáveis

---
+++ 10 - Typescript/02/index.js
@@ -0,0 +1,12 @@
+"use strict";
+const personName = 'Daniel';
+const personAge = 22;
+const personAlive = true;
+const personContacts = [
+    'daniel.bonifacio@algaworks.com',
+    'daniel.bonifacio@algaworks.com'
+];
+const personContacts2 = [
+    0,
+    'daniel.bonifacio@algaworks.com',
+];
---
+++ 10 - Typescript/02/index.ts
@@ -0,0 +1,11 @@
+const personName:string = 'Daniel'
+const personAge:number = 22
+const personAlive:boolean = true
+const personContacts:string[] = [
+    'daniel.bonifacio@algaworks.com',
+    'daniel.bonifacio@algaworks.com'
+]
+const personContacts2:(string | number)[] = [
+    0,
+    'daniel.bonifacio@algaworks.com',
+]
