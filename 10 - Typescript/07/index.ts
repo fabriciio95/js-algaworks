201709c0a87067c150d824d2558c6bb8b0351f75
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-07-29 17:38:56 -0300

    Interfaces

---
+++ 10 - Typescript/07/index.ts
@@ -0,0 +1,15 @@
+interface Animal {
+    sex: 'male' | 'female'
+}
+
+
+interface Human extends Animal {
+    name: string,
+    age: number
+}
+
+const person: Human = {
+    age: 22,
+    name: 'Lucas',
+    sex: 'male'
+}
